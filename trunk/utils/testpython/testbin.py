32e422588a96bfc0e805b067a8aef9a620b49343
baden.i.ua <baden.i.ua@21386812-f4a7-11de-b019-c7d0bc33d736>
2010-05-21 22:21:15 +0000

    Regular update, backup.

--- trunk/utils/testpython/testbin.py
+++ trunk/utils/testpython/testbin.py
@@ -3,17 +3,50 @@
 # testbin.py
 
 import httplib
+import math
 #import urllib
 
-def main():
-	text = "abcdefghijklmnopqrstuvwxyz"
-	print text
+def dec2angleo(x, y):
+	if x==0 and y==0:
+		return -1.0
+	if y==0:
+		if x>0.0: return 270
+		else: return 90
+	theta = math.atan(y/x)
+	theta = theta * 180 / math.pi
+	if x>0.0:
+		if y>0.0: return theta
+		else: return 360 + theta
+	else:
+		return 180+theta
 
-	while len(text)>0:
-		print text[:8]
-		text = text[8:]
-		#print text
-	#print text
+def dec2angle(x, y):
+	if x==0 and y==0:
+		return -1.0
+	if y==0:
+		if x>0.0: return 270
+		else: return 90
+	theta = math.atan(x/y)
+	theta = theta * 180 / math.pi
+	if y>0.0:
+		if x>0.0: return 360-theta
+		else: return -theta
+	else:
+		return 180-theta
+
+
+def test(x, y):
+	print "test(%6.3f, %6.3f) = %6.3f" % (x, y, dec2angle(x, y))
+
+def main():
+	test(0.0, 1.0)
+	test(-1.0, 1.0)
+	test(-1.0, 0.0)
+	test(-1.0, -1.0)
+	test(0.0, -1.0)
+	test(1.0, -1.0)
+	test(1.0, 0.0)
+	test(1.0, 1.0)
 
 	
 
