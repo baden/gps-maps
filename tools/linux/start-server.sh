1c84d80b85cd5524ae14285da272f7456d652a15
baden.i.ua <baden.i.ua@21386812-f4a7-11de-b019-c7d0bc33d736>
2010-10-15 21:26:35 +0000

    Create linux-tools

---
+++ tools/linux/start-server.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+python2.5 /home/baden/SDK/google_appengine/dev_appserver.py --port=8080 --datastore_path=./datasotre ../gps-maps/
---
+++ tools/linux/upload-gps-maps.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+python2.5 /home/baden/SDK/google_appengine/appcfg.py --email=baden.i.ua@gmail.com update ../gps-maps/
